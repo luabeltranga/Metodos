2d3ef1fec9dfed60dc8192a98940c2deca1bb1ab
luabeltranga <luabeltranga@unal.edu.co>
2017-02-10 08:35:42 -0800

    20/02/2017

---
+++ tercera_clase/string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+  std::string mystring;
+  std::cout<<mystring<<std::endl;
+
+  mystring = "huyhuyhuyhjuyyhh u uj" ;
+  std::cout<<mystring<<std::endl;
+
+  return 0;
+}
+
+
+/* libreria string esta en la libreria estandar  */
---
+++ tercera_clase/vars.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(void)
+{
+  int a= 0;
+  int b {2};
+  auto c= b;
+
+  
+  std::cout<<a<<std::endl;
+  std::cout<<b<<std::endl;
+  std::cout<<c<<std::endl;
+
+
+
+
+
+
+  return 0;
+}
+
+
+/* asignacion de valores iniciales a las variables int b {3} */
+
+// auto para asignar el mismo tipo de una variable a otra
