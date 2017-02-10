fdaebbabc0e1e52bd7bc7af86b4658681805dabc
luabeltranga <luabeltranga@unal.edu.co>
2017-02-10 05:12:13 -0500

    08/02

--- segunda_clase/tipos_de_salida.cpp
+++ segunda_clase/tipos_de_salida.cpp
@@ -8,3 +8,8 @@ int main()
 
   return 0;
 }
+/*al ejecutar el archivo .out se pueden crear dos tipos de archivos, uno con 
+  los datos "cout"  con 1> y los errores "cerr" con 2> */
+
+
+// al ejecutar $? luego de ejecutar se obtiene en consola el return
---
+++ segunda_clase/tipos_de_salida.cpp~
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  std::cout<<"Hola mundo"<<std::endl;
+  std::cerr<<"Error"<<std::endl;
+  
+
+  return 0;
+}
