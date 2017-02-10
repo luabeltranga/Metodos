70053285dea2bc7cbff8cde13aa194d5c712b392
luabeltranga <luabeltranga@unal.edu.co>
2017-02-10 08:57:59 -0800

    10/02

--- tercera_clase/string.cpp
+++ tercera_clase/string.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
 
+const double  pi= 3.14159;
+
 int main(void)
 {
   std::string mystring;
   std::cout<<mystring<<std::endl;
 
-  mystring = "huyhuyhuyhjuyyhh u uj" ;
+  mystring = "huyhuyhuyhjuyyhh u uj \a " ;
   std::cout<<mystring<<std::endl;
 
   return 0;
@@ -14,3 +16,4 @@ int main(void)
 
 
 /* libreria string esta en la libreria estandar  */
+/* variables globales se declaran fuera de la funcion main */
